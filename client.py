9b6e63a14021dc7623796aeac600d94e1f45615b
Jaap <jaap@blinkinglights.nl>
2020-09-17 20:30:51 +0200

    First test

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+PACKAGE = MESSAGE.encode('utf-8')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(PACKAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print("received data: {}".format(data))
---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print('\nConnection address: {}'.format(addr))
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    message = data.decode('utf-8')
+    print("received data: {}".format(data))
+    conn.send(data)  # echo
+    for i in range(100):
+        conn.send(i.to_bytes(1, byteorder='big'))
+
+conn.close()
